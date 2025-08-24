d35fa909d1851f60e66e05e0273a9212f2275fca
shemehrotra_deloitte <shemehrotra@deloitte.com>
2025-08-25 01:48:53 +0530

    added data binding

---
+++ data-binding/src/app/app.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [FormsModule],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+// For Interpolation
+title: string = 'Angular 20 Data Binding Example';
+
+// For Property Binding
+imageUrl: string = 'https://angular.io/assets/images/logos/angular/angular.png';
+isDisabled: boolean = true;
+
+// For Event Binding
+counter: number = 0;
+
+incrementCounter() {
+this.counter++;
+}
+
+// For Two-way Binding
+username: string = '';
+}
